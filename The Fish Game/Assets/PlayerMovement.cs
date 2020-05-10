2c9441bd904b7e6d3d79292914001c73f439c99d
lalaman744 <31625784+lalaman744@users.noreply.github.com>
2020-05-10 13:47:46 -0500

    Idle Animations up down left right

--- The Fish Game/Assets/PlayerMovement.cs
+++ The Fish Game/Assets/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     Vector2 movement;
 
+
     // Update is called once per frame
     void Update()
     {
@@ -18,6 +19,24 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (movement.x == -1)
+        {
+            animator.SetInteger("Direction", 4);
+            Debug.Log("moving left");
+        } else if (movement.x == 1)
+        {
+            animator.SetInteger("Direction", 2);
+            Debug.Log("moving right");
+        } else if (movement.y == 1)
+        {
+            animator.SetInteger("Direction", 1);
+            Debug.Log("Moving Up");
+        } else if (movement.y == -1)
+        {
+            animator.SetInteger("Direction", 3);
+            Debug.Log("Moving Down");
+        }
     }
 
     void FixedUpdate()
